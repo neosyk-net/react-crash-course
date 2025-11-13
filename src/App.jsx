fd9a1082eef8f772439ebd067500f5f270add08b
neosyk-net <runerdycastillo@gmail.com>
2025-11-12 22:14:13 -0500

    React Course Finish!

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
   return (
   <div>
     <Router>
-      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users/:id" element={<Users />} />
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,6 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Users() {
     const { id } = useParams();
@@ -8,6 +9,7 @@ function Users() {
     async function fetchUser() {
         const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
         console.log(data)
+        setUser(data)
     }
         
     
@@ -15,7 +17,15 @@ function Users() {
     useEffect(() => {
        fetchUser();
     }, []);
-    return <h1>{id}</h1>
+    return (
+        <div>
+            <Link to="/">Back</Link>
+            <h1>{ user.id }</h1>
+            <h1>{ user.name }</h1>
+            <h1>{ user.email }</h1>
+            <h1>{ user.username }</h1>
+        </div>
+    )
 }
 
 export default Users;
